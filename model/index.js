8f545c792c3c49456bb718a6bc8473e015b11fb4
Maggie Kasunic <maggie.kasunic@gmail.com>
2021-01-23 17:16:34 -0600

    create and delete endpoints

--- model/index.js
+++ model/index.js
@@ -1,84 +1,175 @@
-const { Color, Size, Quantity, Shoecolor, Shoesize } = require('../db/index.js');
+const { Color, Size, Quantity, Shoecolor, Shoesize, Shoe } = require('../db/index.js');
 const { Op } = require('sequelize');
 
+let randomNumberGenerator = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-let colors = (id) => {
-  return new Promise((resolve, reject) => {
-    Shoecolor.findAll({
-      where: {
-        shoe_id: id
-      }
-    })
-    .then (shoeColors => {
-      return shoeColors.map(x => x.dataValues.color_id);
-    })
-    .then(colorIDs => {
-      Color.findAll({
+let get = {
+  colors: (id) => {
+    return new Promise((resolve, reject) => {
+      Shoecolor.findAll({
         where: {
-          id: {
-            [Op.or]: colorIDs
+          shoe_id: id
+        }
+      })
+      .then (shoeColors => {
+        return shoeColors.map(x => x.dataValues.color_id);
+      })
+      .then(colorIDs => {
+        Color.findAll({
+          where: {
+            id: {
+              [Op.or]: colorIDs
+            }
           }
+        })
+        .then(results => {
+          resolve(results.map(x => x.dataValues));
+        })
+      })
+      .catch(err => {
+        reject(err);
+      });
+    });
+  },
+  sizes: (id) => {
+    return new Promise((resolve, reject) => {
+      Shoesize.findAll({
+        where: {
+          shoe_id: id
+        }
+      })
+      .then (shoesizes => {
+        return shoesizes.map(x => x.dataValues.size_id);
+      })
+      .then(sizeIDs => {
+        Size.findAll({
+          where: {
+            id: {
+              [Op.or]: sizeIDs
+            }
+          }
+        })
+        .then(results => {
+          resolve(results.map(x => x.dataValues));
+        })
+      })
+      .catch(err => {
+        reject(err);
+      });
+    });
+  },
+  quantity: (shoeID, colorID) => {
+    return new Promise((resolve, reject) => {
+      Quantity.findAll({
+        where: {
+          shoe_id: shoeID,
+          color_id: colorID
         }
       })
       .then(results => {
-        resolve(results.map(x => x.dataValues));
+        resolve(results.map(x => ({ size_id: x.dataValues.size_id, quantity: x.dataValues.quantity })));
       })
-    })
-    .catch(err => {
-      reject(err);
+      .catch(err => {
+        reject(err);
+      });
     });
-  });
-};
-
-let sizes = (id) => {
-  return new Promise((resolve, reject) => {
-    Shoesize.findAll({
-      where: {
-        shoe_id: id
-      }
-    })
-    .then (shoesizes => {
-      return shoesizes.map(x => x.dataValues.size_id);
-    })
-    .then(sizeIDs => {
-      Size.findAll({
-        where: {
-          id: {
-            [Op.or]: sizeIDs
-          }
-        }
-      })
-      .then(results => {
-        resolve(results.map(x => x.dataValues));
-      })
-    })
-    .catch(err => {
-      reject(err);
-    });
-  });
-};
-
-let quantity = (shoeID, colorID) => {
-  return new Promise((resolve, reject) => {
-    Quantity.findAll({
-      where: {
-        shoe_id: shoeID,
-        color_id: colorID
-      }
-    })
-    .then(results => {
-      resolve(results.map(x => ({ size_id: x.dataValues.size_id, quantity: x.dataValues.quantity })));
-    })
-    .catch(err => {
-      reject(err);
-    });
-  });
-};
-
-module.exports = {
-  get: {
-    colors: colors,
-    sizes: sizes,
-    quantity: quantity
   }
 }
+
+let create = (details) => {
+  let id;
+  return Shoe.create({
+    name: details.name,
+    model: details.model
+  })
+  .then((shoe) => {
+    id = shoe.dataValues.id
+    let bulkColors = [];
+    details.color.forEach(item => {
+      bulkColors.push({ shoe_id: shoe.dataValues.id, color_id: item })
+    })
+    return Shoecolor.bulkCreate(bulkColors);
+  })
+  .then((shoe) => {
+    let bulkSize = [];
+    details.size.forEach(item => {
+      bulkSize.push({ shoe_id: id, size_id: item })
+    })
+    return Shoesize.bulkCreate(bulkSize);
+  })
+  .then((shoe) => {
+    let bulkQuantity = [];
+    details.quantity.forEach(item => {
+      bulkQuantity.push({ shoe_id: id, color_id: item.color, size_id: item.size, quantity: item.quantity })
+    })
+    return Quantity.bulkCreate(bulkQuantity);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+};
+
+let update = () => {
+
+};
+
+let recycle = (number) => {
+  return Quantity.destroy({
+    where: {
+      shoe_id: number
+    }
+  })
+  .then(() => {
+    Shoecolor.destroy({
+      where: {
+        shoe_id: number
+      }
+    })
+  })
+  .then(() => {
+    Shoesize.destroy({
+      where: {
+        shoe_id: number
+      }
+    })
+  })
+  .then(() => {
+    Shoe.destroy({
+      where: {
+        id: number
+      }
+    })
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
+
+module.exports = {
+  get: get,
+  create: create,
+  update: update,
+  recycle: recycle
+}
+
+// colors                                |
+// | quantities                            |
+// | shoecolors                            |
+// | shoes                                 |
+// | shoesizes                             |
+// | sizes
+
+// select * from quantities;
+// +-------+---------+----------+---------+----------+
+// | id    | shoe_id | color_id | size_id | quantity |
+// +-------+---------+----------+---------+----------+
+// |     2 |       1 |        1 |       7 |        0 |
+// |     3 |       1 |        1 |       8 |        1 |
+// |     4 |       1 |        1 |       9 |        4 |
+// |     5 |       1 |        1 |      10 |        3 |
+// |     6 |       1 |        1 |      11 |        2 |
+// |     7 |       1 |        1 |      12 |        8 |
+// |     8 |       1 |        1 |      13 |        6 |
+// |     9 |       1 |        1 |      14 |        4 |
--- server/index.js
+++ server/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = '3001';
 const shoes = require('../model');
+var bodyParser = require('body-parser')
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 
 app.get('/shoes/:shoeId/colors', (req, res) => {
   let shoeId = req.params.shoeId;
@@ -39,6 +43,50 @@ app.get('/shoes/:shoeId/colors/:colorId/quantities', (req, res) => {
     console.error(err);
     res.end();
   });
+});
+
+
+
+/////////// CRUD
+
+ // Create a new shoe
+app.post('/shoes', (req, res) => {
+  shoes.create(req.body)
+  .then(result => {
+    res.send(result);
+  })
+  .catch(err => {
+    console.log(err);
+    res.end;
+  })
+});
+
+ // Retrieve all shoes
+ app.get('/shoes', (req, res) => {
+   console.log('getall')
+   res.end()});
+
+ // Retrieve a single shoe
+ app.get('/shoes/:shoeId', (req, res) => {
+   console.log('get')
+   res.end()});
+
+ // Update a shoe
+ app.put('/shoes/:shoeId', (req, res) => {
+   console.log('update')
+   res.end()});
+
+ // Delete a shoe
+app.delete('/shoes/:shoeId', (req, res) => {
+  let id = req.params.shoeId;
+  shoes.recycle(id)
+  .then(() => {
+    res.end();
+  })
+  .catch(err => {
+    console.error(err);
+    res.end();
+  })
 });
 
 app.listen(PORT, () => {
